bf0bbbbaf406996c5e33f502551ed5c1f24de50c
alanashannon <alashannon@gmail.com>
2020-12-07 17:29:37 -0800

    Add sessions controller methods and jbuilder files

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -2,10 +2,21 @@ class Api::SessionsController < ApplicationController
 
     #login
     def create
-        
+        @user = User.find_by_credentials(params[:user][:email], params[:user][:password])
+        if @user.nil?
+            render json: ['Invalid credentials'], status: 401
+        else
+            login!(@user)
+            render 'api/users/show'
+        end
     end
 
     #logout
     def destroy
+        logout! if logged_in?
+        render {} 
+        if !current_user
+            render json: ['No user!'], status: 404
+        end
     end
 end
