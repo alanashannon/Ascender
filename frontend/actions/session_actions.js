35c2ec7d50959cec1bfb6639fbb6e4eb29cf3178
alanashannon <alashannon@gmail.com>
2020-12-07 23:29:09 -0800

    Add session actions

---
+++ frontend/actions/session_actions.js
@@ -0,0 +1,36 @@
+import * as SessionAPIUtil from '../util/session_api_util'; 
+
+export const RECEIVE_CURRENT_USER = 'RECEIVE_CURRENT_USER';
+export const LOGOUT_CURRENT_USER = 'LOGOUT_CURRENT_USER'; 
+export const RECEIVE_ERRORS = 'RECEIVE_ERRORS';
+
+//action creators
+const receiveCurrentUser = (currentUser) => ({
+    type: RECEIVE_CURRENT_USER, 
+    currentUser
+});
+
+const logoutCurrentUser = () => ({
+    type: LOGOUT_CURRENT_USER, 
+});
+
+const receiveErrors = (errors) => ({
+    type: RECEIVE_ERRORS, 
+    errors
+});
+
+//thunk actions
+export const login = (user) => dispatch => (
+    SessionAPIUtil.login(user)
+        .then(user => dispatch(receiveCurrentUser(user)))
+);
+
+export const logout = () => dispatch => (
+    SessionAPIUtil.logout()
+        .then(user => dispatch(logoutCurrentUser()))
+);
+
+export const signup = (user) => disppatch => (
+    SessionAPIUtil.signup(user)
+        .then(user => dispatch(receiveCurrentUser(user)))
+);
