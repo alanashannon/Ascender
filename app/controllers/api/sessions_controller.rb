5da4b689a85921a5e90b12f80efa36c2a95f6d1a
alanashannon <alashannon@gmail.com>
2020-12-07 17:20:20 -0800

    Add users controller methods

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,8 +1,11 @@
 class Api::SessionsController < ApplicationController
 
+    #login
     def create
+        
     end
 
+    #logout
     def destroy
     end
 end
--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,11 +1,28 @@
 class Api::UsersController < ApplicationController
 
     def new
+        @user = User.new 
+        render :new 
     end
 
+    #signup
     def create 
+        @user = User.new(user_params)
+        if @user.save
+            login!(@user)
+            render :show
+        else
+            render json: @user.errors.full_messages, status: 401
+        end
     end
 
     def index
+        @users = User.all 
+        render :index 
+    end
+
+    private 
+    def user_params
+        params.require(:user).permit(:name, :email, :password, :location, :biography)
     end
 end
