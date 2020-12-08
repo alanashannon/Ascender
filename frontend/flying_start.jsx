85a87f295ed00a86a5c294d699904154129ffc5e
alanashannon <alashannon@gmail.com>
2020-12-07 16:12:13 -0800

    Add users table

--- frontend/flying_start.jsx
+++ frontend/flying_start.jsx
@@ -1,8 +1,8 @@
 import React from "react"; 
-import ReactDOM from "react-dom"
+import ReactDOM from "react-dom";
 
 document.addEventListener("DOMContentLoaded", () => {
     const root = document.getElementById('root'); 
-    ReactDOM.render(<h1>Welcome to Flying Start</h1>, root)
+    ReactDOM.render(<h1>Welcome to Flying Start</h1>, root);
 })
 
