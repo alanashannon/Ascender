5e3ebb8a99138516395a17ef1589961594369e8a
alanashannon <alashannon@gmail.com>
2020-12-10 00:05:50 -0800

    Edit header css

--- frontend/components/greeting/greeting.jsx
+++ frontend/components/greeting/greeting.jsx
@@ -4,12 +4,12 @@ import { Link } from 'react-router-dom';
 const Greeting = ({ currentUser, logout }) => {
     const newSession = () => (
         <nav className="login-nav-link">
-            <Link to="/login">Log In</Link>
+            <Link to="/login">Log in</Link>
         </nav>
     );
     const greetingMessage = () => (
         <nav>
-            <h2>Hello, {currentUser.name}</h2>
+            <h2 className="logged-in-greeting">Hello, {currentUser.name}</h2>
             <button onClick={logout}>Log Out</button>
         </nav>
     );
