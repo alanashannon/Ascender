3381764da647388c044784160f7368be25574ee3
alanashannon <alashannon@gmail.com>
2020-12-11 18:12:11 -0800

    Add project actions

---
+++ frontend/actions/project_actions.js
@@ -0,0 +1,6 @@
+import * as ProjectAPIUtil from '../util/project_api_util';
+
+export const RECEIVE_ALL_PROJECTS = "RECEIVE_ALL_PROJECTS";
+export const RECEIVE_PROJECT = "RECEIVE_PROJECT";
+export const REMOVE_PROJECT = "REMOVE_PROJECT";
+
