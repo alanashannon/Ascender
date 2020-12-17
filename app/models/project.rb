8e591af64436095e9d92621be3a9d09c37069bfc
alanashannon <alashannon@gmail.com>
2020-12-17 10:40:45 -0800

    Add associations and validations

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,10 +1,19 @@
 class Project < ApplicationRecord
     validates :title, :description, :end_date, :campaign, :funding_goal, :amount_pledged, :risks, presence: true
-    # add validation/association later for category id
+    #TODO add validation/association later for category id
+
     belongs_to :author, 
         primary_key: :id,
         foreign_key: :author_id, 
         class_name: :User
+
+    has_many :backings, 
+        foreign_key: :project_id, 
+        class_name: :Backing 
+
+    has_many :rewards, 
+        foreign_key: :project_id, 
+        class_name: :Reward 
 
     has_one_attached :photo
 
