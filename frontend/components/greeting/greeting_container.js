f53e3ebcf48afbfc3a3898ae316dcc6fedb9cd66
alanashannon <alashannon@gmail.com>
2021-02-26 15:23:57 -0800

    Add user's projects to dropdown menu

--- frontend/components/greeting/greeting_container.js
+++ frontend/components/greeting/greeting_container.js
@@ -1,15 +1,19 @@
 import { connect } from 'react-redux'; 
 import Greeting from './greeting';
 import { logout } from '../../actions/session_actions';
+import { fetchProjects } from '../../actions/project_actions'
 
-const mapStateToProps = ({ session, entities: { users } }) => {
+const mapStateToProps = ({ session, entities: { users, projects } }) => {
     return {
-        currentUser: users[session.id]
+        currentUser: users[session.id],
+        users: users, 
+        projects: projects
     }
 };
 
 const mapDispatchToProps = (dispatch) => ({
-    logout: () => dispatch(logout())
+    logout: () => dispatch(logout()),
+    fetchProjects: () => dispatch(fetchProjects())
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(Greeting)
