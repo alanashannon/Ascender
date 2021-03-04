a35f0540bdb1d8c7922cdb912bea6a6492980b2d
alanashannon <alashannon@gmail.com>
2021-03-03 18:30:16 -0800

    Add page arrows

--- frontend/components/projects/project_index.jsx
+++ frontend/components/projects/project_index.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import ProjectIndexItem from './project_index_item';
 import FeaturedProject from './featured_project';
-
+import { IoIosArrowBack, IoIosArrowForward } from 'react-icons/io'; 
 
 class ProjectIndex extends React.Component {
     constructor(props) {
@@ -18,13 +18,16 @@ class ProjectIndex extends React.Component {
     componentDidMount() {
         this.props.fetchProjects(); 
         this.props.fetchUsers();  
-        this.props.fetchCategories(); 
+        this.props.fetchCategories();
+
+        let firstPage = document.getElementById("1")
+        firstPage.style.fontWeight = "bold" 
     }
 
     handlePage(e) {
         e.preventDefault(); 
         const clicked = e.target 
-        
+
         this.setState({
             pageNum: clicked.id, 
         })
@@ -32,20 +35,27 @@ class ProjectIndex extends React.Component {
         let page1 = document.getElementById("1")
         let page2 = document.getElementById("2")
         let page3 = document.getElementById("3")
+        let arrowLeft = document.getElementById("arrow-left")
+        let arrowRight = document.getElementById("arrow-right")
 
         if (clicked.id === "1") {
-            console.log("clicked")
             page1.style.fontWeight = "bold"
             page2.style.fontWeight = "normal"
             page3.style.fontWeight = "normal"
+            arrowLeft.style.color = "gray"
+            arrowRight.style.color = "green"
         } else if (clicked.id === "2") {
             page2.style.fontWeight = "bold"
             page1.style.fontWeight = "normal"
             page3.style.fontWeight = "normal"
+            arrowLeft.style.color = "green"
+            arrowRight.style.color = "green"
         } else if (clicked.id === "3") {
             page3.style.fontWeight = "bold"
             page1.style.fontWeight = "normal"
             page2.style.fontWeight = "normal"
+            arrowLeft.style.color = "green"
+            arrowRight.style.color = "gray"
         }
     }
 
@@ -56,6 +66,8 @@ class ProjectIndex extends React.Component {
         let result = projectSlice.map((project, i) => {
             return <ProjectIndexItem project={project} users={this.props.users} key={i} />
         })
+
+        
 
         return (
             <div>
@@ -83,9 +95,11 @@ class ProjectIndex extends React.Component {
                             <br />
                         </ul>
                         <div className="pageNum-list">
+                            <div className="pageArrow"><IoIosArrowBack size={16} color={"gray"} id="arrow-left"/></div>
                             <div onClick={this.handlePage} className="pageNum" id="1" >1</div>
                             <div onClick={this.handlePage} className="pageNum" id="2" >2</div>
                             <div onClick={this.handlePage} className="pageNum" id="3" >3</div>
+                            <div className="pageArrow"><IoIosArrowForward size={16} color={"green"} id="arrow-right"/></div>
                         </div>
                     </section>
                 </div>
