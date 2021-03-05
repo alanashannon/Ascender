2d58b8e6cc72bd37d155980743bca7f7e34992d6
alanashannon <alashannon@gmail.com>
2021-03-05 11:47:02 -0800

    Add search results details

--- frontend/components/search/search_results.jsx
+++ frontend/components/search/search_results.jsx
@@ -1,6 +1,7 @@
 import React from 'react'; 
 import { Link } from 'react-router-dom';
 import { IoIosPin } from 'react-icons/io';
+import { MdErrorOutline } from 'react-icons/md';
 
 class SearchResults extends React.Component {
     constructor(props) {
@@ -23,13 +24,58 @@ class SearchResults extends React.Component {
         if (projectsList.length === 0) {
             categoriesList = categoriesList.filter((category) => category.category_name.toLowerCase().includes(this.props.input.toLowerCase()));
             this.props.projects.forEach((project) => {
-                if (project.category_id === categoriesList[0].id) {
+                if ((categoriesList.length > 0) && (project.category_id === categoriesList[0].id)) {
                     categoryProjects.push(project) 
                 }
             })
         }
-        // console.log(categoryProjects)
-        // console.log(projectsList)
+        
+        let noResults = <div>
+            <div className="no-results-message-container">
+                <div className="message-container">
+                    <div className="error-icon"><MdErrorOutline size={18}/></div>
+                    <div className="no-results-message1">We can't find projects that match your search</div>
+                </div>
+                <div className="no-results-message2">Check out a collection of popular and recommended options below</div>
+            </div>
+            <div className="more-projects">More projects coming to life</div>
+            <div className="search-results-container">
+                {this.props.projects.slice(4, 10).map((project, k) => {
+                    return (
+                        <div key={k} className="project-search-each">
+                            <Link to={`/projects/${project.id}`}>
+                                <img src={project.photo} />
+                            </Link>
+                            <div className="project-search-info">
+                                <div className="project-search-title">
+                                    <Link className="project-search-title" to={`/projects/${project.id}`}>
+                                        {project.title}
+                                    </Link>
+                                </div>
+                                <div className="project-search-desc">
+                                    {project.description}
+                                </div>
+                                <div className="project-search-name">
+                                    by {this.props.users[project.author_id].name}
+                                </div>
+                                <div className="project-search-progress-bar-container">
+                                    <div className="project-search-show-progress-bar" style={{ width: `calc(1% * ${Math.floor((project.amount_pledged / project.funding_goal) * 100)})` }}></div>
+                                </div>
+                                <div className="project-search-amt">
+                                    ${project.amount_pledged} pledged
+                                    </div>
+                                <div className="project-search-days-left">
+                                    {!daysLeft ? null : daysLeft(project.end_date) < 0 ? "0" : daysLeft(project.end_date).toString()} days left
+                                    </div>
+                                <div className="project-search-location">
+                                    <IoIosPin size={14} className="map-pin" /> {project.location}
+                                </div>
+                            </div>
+                        </div>
+                    )
+                })}
+            </div>
+        </div>
 
         let daysLeft = (date) => {
             let today = new Date();
@@ -49,6 +95,17 @@ class SearchResults extends React.Component {
                     <div className="search-intro-input">Earth</div>
                     <div className="search-intro">sorted by</div>
                     <div className="search-intro-input">Magic</div>
+                </div>
+                <div>
+                    {(projectsList.length > 0) ? 
+                    <div className="explore-message-container">
+                        <div className="explore">Explore</div>&nbsp;
+                        <div className="num-projects">{projectsList.length} projects</div>
+                    </div> : (categoryProjects.length > 0 & projectsList.length === 0) ? 
+                    <div className="explore-message-container">
+                        <div className="explore">Explore</div>&nbsp;
+                        <div className="num-projects">{categoryProjects.length} projects</div>
+                    </div> : null}
                 </div>
                 <div className="search-results-container">
                     {(projectsList.length > 0) ? projectsList.map((project, i) => {
@@ -119,6 +176,7 @@ class SearchResults extends React.Component {
                         )
                     })}
                 </div>
+                {(categoryProjects.length === 0 && projectsList.length === 0) ? noResults : null}
             </div>
         )
     }
